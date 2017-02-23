967f52b3c9281a175d3bbd1bcd3323cfca73765a
lichen <chenlee1024@gmail.com>
2017-02-23 18:16:59 +0800

    add template

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,13 @@
             ,   classes = SETTINGS._classes
             ,   url = SETTINGS.url
             ,   limit = SETTINGS.limit
+            ,   thumb = SETTINGS.thumb
+            ,   append = SETTINGS.fileListAppend
+            ,   flContainerTpl = SETTINGS.fileListContainer
+            ,   flItemTpl = SETTINGS.fileListItem
+            ,   flContainer = classes.fileContainer
+            ,   flItem = classes.fileItem
+            ,   flThumb = classes.thumb
             ,   doneCallback = SETTINGS.done
             ,   _this = $(this)
             ,   _form = _this.parents("form")
@@ -35,7 +42,9 @@
                         .on('click', classes.button, function() {
                             _this
                                 .trigger('click')
+                                .off('change')
                                 .on('change', function() {
+                                    self._initFileListTpl();
                                     self._handleFiles.call(this);
                                 });
                         });
@@ -43,7 +52,8 @@
                     _handleFiles : function() {
                         var files = this.files
                         ,   fileLen = files.length
-                        ,   formData = new FormData;
+                        ,   formData = new FormData
+                        ,   self = this;
                         if (fileLen > limit) {
                             alert("最多只能上传" + limit + "个文件");
                             return;
@@ -51,6 +61,7 @@
                         $(files).each(function(i, ifile) {
                             if (ifile) {
                                 formData.append('__files[]', ifile);
+                                uuu._selectFilesTpl(ifile);
                             }
                         });
                         uuu._uploadFiles(formData);
@@ -68,9 +79,37 @@
                            if ($.isFunction(doneCallback)) {
                                doneCallback.call(null, res);
                            } else {
-                               alert('Done is not a function!');
+                               console.log(res);
                            }
+                        }).catch(function(err){
+                            if ($.isFunction(error)) {
+                                error.call(null, err);
+                            } else {
+                                console.log(err);
+                            }
                         });
+                    },
+                    _initFileListTpl : function() {
+                        $(append).append(flContainerTpl);
+                        $(flContainer).append(flItemTpl);
+                    },
+                    _selectFilesTpl : function(file) {
+                        var reader = new FileReader();
+
+                        $(classes.name).text(file.name);
+
+                        // 是否显示缩略图
+
+                        reader.onload = function(e) {
+                            if (thumb) {
+                                // support thumb
+                                $(flThumb).find("img").attr("src",e.target.result);
+                            } else {
+                                // not support thumb
+                                $(flThumb).find("img").remove();
+                            }
+                        }
+                        reader.readAsDataURL(file);
                     }
                 };  
 
@@ -94,13 +133,22 @@
     $.fn.filer.DEFAULTS = {
             limit : 1,
             url : '',
+            thumb : true,
             fileButton : "#k-file-input",
-            fileTpl : "<div class=\"k-files-container\"><div class=\"k-file-fake-input\"><div class=\"k-file-input-button\"></div></div></div>",
+            fileTpl : "<div class=\"k-files-container\"><div class=\"k-file-fake-input\"><div class=\"k-file-input-button\"></div><div class=\"k-files\"></div></div></div>",
+            fileListAppend : ".k-files",
+            fileListContainer : "<ul class=\"k-files-items\"></ul>",
+            fileListItem : "<li class=\"k-file-item\"><div class=\"k-file-item-container\"><div class=\"k-file-item-thumb\"><img src=\"\" alt=\"\"></div><div class=\"k-file-item-name\"></div><a href=\"javascript:;\" class=\"k-file-delete\">X</a></div></li>",
             buttonText : "选择文件",
             done : null,
             _classes : {
                 container : ".k-files-container",
-                button : ".k-file-input-button"
+                button : ".k-file-input-button",
+                fileContainer : ".k-files-items",
+                fileItem : '.k-file-item',
+                thumb : '.k-file-item-thumb',
+                name : '.k-file-item-name',
+                delete : '.k-file-delete'
             }
     };
 
